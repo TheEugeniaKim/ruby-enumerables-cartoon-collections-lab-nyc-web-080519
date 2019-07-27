39a914ade6bdae38263971ef8a74d46bba24ba52
Eugenia Kim <elk303@nyu.edu>
2019-07-27 03:49:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,32 +1,69 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |element,index|
-    puts "#{index + 1 } #{element}"
-  end
+  array.each_with_index { |element, index| 
+  puts "#{index +1} #{element}"
+  }
 end
+  
 
 def summon_captain_planet(array)
-  array.collect{ |element| element.capitalize + "!"}
+  array.map { |element| element.capitalize + "!"}
+end
+  
+def long_planeteer_calls(array)
+  array.each do |element| 
+  if element.length > 4 
+    return true 
+  end
+ end
+ return false 
 end
 
-def long_planeteer_calls(array)
-  array.each do |word|
-    if word.length > 4 
-    return true 
-    end
-  end
-false 
-end 
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.each do |element|
+  array.each do |element| 
     if cheese_types.include?(element)
-      return element
-    end
+  return element 
   end
- nil 
 end
+
+
+
+
+
+
+
+
+
+# def roll_call_dwarves(array)
+#   array.each_with_index do |element,index|
+#     puts "#{index + 1 } #{element}"
+#   end
+# end
+
+# def summon_captain_planet(array)
+#   array.collect{ |element| element.capitalize + "!"}
+# end
+
+# def long_planeteer_calls(array)
+#   array.each do |word|
+#     if word.length > 4 
+#     return true 
+#     end
+#   end
+# false 
+# end 
+
+# def find_the_cheese(array)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+#   array.each do |element|
+#     if cheese_types.include?(element)
+#       return element
+#     end
+#   end
+# nil 
+# end
 
 
 
